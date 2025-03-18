a333400acbf51a1869158ab149a5a56547cfc7db
GKamundia <gkamush50@gmail.com>
2025-03-18 12:28:45 +0300

    removed duplicates

--- check_tables.py
+++
@@ -1,7 +0,0 @@
-import lancedb
-
-db = lancedb.connect("data/lancedb")
-table = db.open_table("docling_tables")
-
-print(f"Total chunks: {table.count_rows()}")
-print(f"Tables detected: {table.search().where('metadata.tables.has_table = true').count_rows()}")
--- show_tables.py
+++
@@ -1,8 +0,0 @@
-
-
-import pyarrow.dataset as ds
-
-dataset = ds.dataset("C:/Users/Anarchy/Documents/Data_Science/CEMA/RCEMA/docling/data/lancedb/docling_tables.lance")
-table = dataset.to_table()
-df = table.to_pandas()
-df[df["metadata.tables.has_table"] == True].head(50)
